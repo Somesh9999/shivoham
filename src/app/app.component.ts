56fe7d58a1b3a767f3ee5c4e3a4eb95e0bb8b8a5
Somesh <samchopz5@gmail.com>
2021-01-05 16:06:19 +0530

    added-header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shivoham';
+
+  constructor(library: FaIconLibrary) {
+    // Add an icon to the library for convenient access in other components
+    library.addIconPacks(fas, far);
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  itemHovered:boolean;
+  dropdownImage:string;
+  dropdownContent:string;
+  authStatus=true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  mouseOverLink(event:Event){
+    this.itemHovered=true;
+    var htmlElement= event.target as HTMLElement;
+    if(htmlElement.id=="trendingSong"){
+      //document.getElementById('nav_dropdown').style.background="lightblue url('../assets/stage1.jpg') no-repeat fixed center";
+      this.dropdownImage="../assets/stage1.jpg";
+      this.dropdownContent="Get the top Trending Songs here.."
+    }
+    else if(htmlElement.id=="artists"){
+      //document.getElementById('nav_dropdown').style.backgroundImage="url(../assets/stage1.jpg)";
+      this.dropdownImage="../assets/artist-banner.jpg";
+      this.dropdownContent="Looking For Your Favotite Artist.. Here you go..";
+    }
+    else if(htmlElement.id=="posts"){
+      this.dropdownImage="../assets/posts.jpg";
+      this.dropdownContent="Showcase Your Own Musical Talent Here..";
+    }
+  }
+
+  mouseLeaveLink(event:Event){
+    this.itemHovered=false;
+    this.dropdownImage="";
+    this.dropdownContent="";
+  }
 }
